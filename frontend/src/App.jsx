b3ce4e85db7c01c1c24a017da7af7d64caf0adab
Gurupriyan j <gurupriyanj007@gmail.com>
2024-11-06 01:07:17 +0530

    update the email duplication issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,33 @@
-
-import Home from './containers/Home';
-import CreateBusiness from './containers/CreateBusiness';
-import Business from './containers/Business';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'primereact/resources/themes/saga-blue/theme.css';  // You can change the theme if desired
-import 'primereact/resources/primereact.min.css';          // Core CSS for PrimeReact components
+import Home from './containers/Home'
+import CreateBusiness from './containers/CreateBusiness'
+import Business from './containers/Business'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import 'primereact/resources/themes/saga-blue/theme.css' // You can change the theme if desired
+import 'primereact/resources/primereact.min.css' // Core CSS for PrimeReact components
 
 // PrimeIcons CSS
-import 'primeicons/primeicons.css';         
-import '/src/assets/css/style.css';
-import Template from './containers/Template';
-import PremiumTemplate from './containers/PremiumTemplate';
+import 'primeicons/primeicons.css'
+import '/src/assets/css/style.css'
+import Template from './containers/Template'
+import PremiumTemplate from './containers/PremiumTemplate'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-business" element={<CreateBusiness />} />
@@ -22,7 +36,7 @@ function App() {
         <Route path="/template/premium/:id" element={<PremiumTemplate />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/Functions/functions.jsx
+++ frontend/src/Functions/functions.jsx
@@ -2,176 +2,216 @@
 import axios from 'axios'
 
 const config = {
-    headers: {
-        'Content-Type': 'application/json',
-    },
-};
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
 
-const baseUrl = 'https://businessbazaarserver.auxxweb.in';
+// const baseUrl = 'https://businessbazaarserver.auxxweb.in'
+const baseUrl = 'http://localhost:5000'
 
 export const fetchCategories = async () => {
-    try {
-        const response = await axios.get(`${baseUrl}/api/v1/category`, config); // Use backticks for template literals
-        
-        if (response.status !== 200) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+  try {
+    const response = await axios.get(`${baseUrl}/api/v1/category`, config) // Use backticks for template literals
 
-        const data = response.data;
-        if (data.success) {
-            return data;
-        } else {
-            console.error("Failed to fetch categories");
-        }
-    } catch (error) {
-        console.error("Error fetching categories:", error.message);
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
     }
-};
+
+    const data = response.data
+    if (data.success) {
+      return data
+    } else {
+      console.error('Failed to fetch categories')
+    }
+  } catch (error) {
+    console.error('Error fetching categories:', error.message)
+  }
+}
 
 export const fetchBusiness = async (page) => {
-    try {
-        const response = await axios.get(`${baseUrl}/api/v1/business?page=${page}&limit=6`, config);
-        
-        if (response.status !== 200) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+  try {
+    const response = await axios.get(
+      `${baseUrl}/api/v1/business?page=${page}&limit=6`,
+      config,
+    )
 
-        const data = response.data; 
-
-        if (data.success) {
-            return data;
-        } else {
-            console.error("Failed to fetch Business");
-        }
-    } catch (error) {
-        console.error("Error fetching Business:", error.message);
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
     }
-};
 
-export const fetchSearchCategory = async (search) => {
-    try {
-        const response = await axios.get(`${baseUrl}/api/v1/category?page=1&limit=10&searchTerm=${search}`, config);
-        
-        if (response.status !== 200) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = response.data;
-
-        if (data.success) {
-            return data;
-
-        } else {
-            console.error("Failed to fetch Search Value");
-        }
-    } catch (error) {
-        console.error("Error fetching Search Value:", error.message);
-    }
-}
-
-export const fetchBusinessTemplate = async (id)=>{
-    try{
-        const response = await axios.get(`${baseUrl}/api/v1/business/${id}`,config)
-
-        if (response.status !== 200) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = response.data;
-        if (data.success){
-            return data
-        }else{
-            console.error('Failed to fetch Search Value')
-        }
-    }catch(error){
-        console.error("Failed to fetch Business Site Details");
-
-    }
-}
-
-
-export const CreateBusinessDetails = async (formData) => {
-    try {
-        const response = await axios.post(`${baseUrl}/api/v1/business/`, formData, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        // if (response.status !== 200) {
-        //     throw new Error(`HTTP error! Status: ${response.status}`);
-        // }
-
-        const data = response.data;
-        if (data.success) {
-            return data; // Successfully created business details
-        } else {
-            console.error('Failed to create business details:', data.message || 'Unknown error');
-            throw new Error(data.message || 'Failed to create business details');
-        }
-    } catch (error) {
-        console.error("Error occurred while fetching business site details:", error.message);
-        throw error; // Propagate the error
-    }
-};
-
-
-
-export const FetchPlans = async () =>{
-    try {
-        const response = await axios.get(`${baseUrl}/api/v1/plans`, config); // Use backticks for template literals
-        
-        if (response.status !== 200) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = response.data;
-        if (data.success) {
-            return data; 
-        } else {
-            console.error("Failed to fetch categories");
-        }
-    } catch (error) {
-        console.error("Error fetching categories:", error.message);
-    }
-}
-
-
-export const getCategoryData = async (categoryId) =>{
-    try{
-        
-        const response = await axios.get(`${baseUrl}/api/v1/category/${categoryId}`, config);
-        
-        if (response.status !== 200){
-            throw new Error('HTTP status: ' + response.status);
-        }
-        const data = response.data;
-
-        if (data.success) {
-            return data
-        }else{
-            console.error("Failed to fetch category data");
-        }
-
-    }catch (error) {
-        console.log("Error fetching category",error.message);
-    }
-}
-
-export const getCategoryBusiness = async (page,categoryId)=>{
-    try{
-        const response = await axios.get(`${baseUrl}/api/v1/business/category/${categoryId}?page=${page}&&limit=6`);
-
-    if (response.status !== 200){
-        throw new Error('HTTP status: ' + response.status);
-    }
-    const data = response.data;
+    const data = response.data
 
     if (data.success) {
-        return data
-    }else{
-        console.log("Failed to fetch business based on category")
+      return data
+    } else {
+      console.error('Failed to fetch Business')
     }
-    }catch(error){
-        console.log("Error fetching business",error.message);
+  } catch (error) {
+    console.error('Error fetching Business:', error.message)
+  }
+}
+
+export const fetchSearchCategory = async (search) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/api/v1/category?page=1&limit=10&searchTerm=${search}`,
+      config,
+    )
+
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
     }
+
+    const data = response.data
+
+    if (data.success) {
+      return data
+    } else {
+      console.error('Failed to fetch Search Value')
+    }
+  } catch (error) {
+    console.error('Error fetching Search Value:', error.message)
+  }
+}
+
+export const fetchBusinessTemplate = async (id) => {
+  try {
+    const response = await axios.get(`${baseUrl}/api/v1/business/${id}`, config)
+
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = response.data
+    if (data.success) {
+      return data
+    } else {
+      console.error('Failed to fetch Search Value')
+    }
+  } catch (error) {
+    console.error('Failed to fetch Business Site Details')
+  }
+}
+
+export const CreateBusinessDetails = async (formData) => {
+  try {
+    const response = await axios.post(`${baseUrl}/api/v1/business/`, formData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    // if (response.status !== 200) {
+    //     throw new Error(`HTTP error! Status: ${response.status}`);
+    // }
+
+    const data = response.data
+    if (data.success) {
+      return data // Successfully created business details
+    } else {
+      console.error(
+        'Failed to create business details:',
+        data.message || 'Unknown error',
+      )
+      throw new Error(data.message || 'Failed to create business details')
+    }
+  } catch (error) {
+    console.error(
+      'Error occurred while fetching business site details:',
+      error.message,
+    )
+    throw error // Propagate the error
+  }
+}
+export const checkBusinessExists = async (formData) => {
+  try {
+    const response = await axios.post(`${baseUrl}/api/v1/business/check`, formData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    // if (response.status !== 200) {
+    //     throw new Error(`HTTP error! Status: ${response.status}`);
+    // }
+
+    const data = response.data
+    if (data.success) {
+      return data // Successfully created business details
+    } else {
+      console.error(
+        'Failed to create business details:',
+        data.message || 'Unknown error',
+      )
+      throw new Error(data.message || 'Failed to create business details')
+    }
+  } catch (error) {
+    console.error(
+      'Error occurred while fetching business site details:',
+      error.message,
+    )
+    throw error // Propagate the error
+  }
+}
+
+export const FetchPlans = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/api/v1/plans`, config) // Use backticks for template literals
+
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const data = response.data
+    if (data.success) {
+      return data
+    } else {
+      console.error('Failed to fetch categories')
+    }
+  } catch (error) {
+    console.error('Error fetching categories:', error.message)
+  }
+}
+
+export const getCategoryData = async (categoryId) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/api/v1/category/${categoryId}`,
+      config,
+    )
+
+    if (response.status !== 200) {
+      throw new Error('HTTP status: ' + response.status)
+    }
+    const data = response.data
+
+    if (data.success) {
+      return data
+    } else {
+      console.error('Failed to fetch category data')
+    }
+  } catch (error) {
+    console.log('Error fetching category', error.message)
+  }
+}
+
+export const getCategoryBusiness = async (page, categoryId) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/api/v1/business/category/${categoryId}?page=${page}&&limit=6`,
+    )
+
+    if (response.status !== 200) {
+      throw new Error('HTTP status: ' + response.status)
+    }
+    const data = response.data
+
+    if (data.success) {
+      return data
+    } else {
+      console.log('Failed to fetch business based on category')
+    }
+  } catch (error) {
+    console.log('Error fetching business', error.message)
+  }
 }
