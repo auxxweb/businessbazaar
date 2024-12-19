e3bec02cba90419b8816da089d8da20f8208c0c6
Sahal Palayat <sahalpalayat5894@gmail.com>
2024-12-19 14:24:28 +0530

    changeurl

--- frontend/src/views/Home/components/BusinessesSection.jsx
+++ frontend/src/views/Home/components/BusinessesSection.jsx
@@ -12,6 +12,12 @@ const BusinessesSection = ({
   totalBusinessData,
   loadMoreBusiness,
 }) => {
+  const slugify = (text) => {
+    return text
+      .toLowerCase()
+      .replace(/ /g, "-") // Replace spaces with hyphens
+      .replace(/[^\w-]+/g, ""); // Remove non-word characters
+  };
   return (
     <section className="home-spot h-auto mb-5">
       <div className="container padding" id="business">
@@ -20,135 +26,136 @@ const BusinessesSection = ({
             Discover the Top Businesses
           </h1>
           <p className="mt-3 text-center" data-aos="fade-left">
-          Explore the most popular business listings in your area through our 
-          local business directory listing, highly rated by locals and visitors alike.
-           Our platform makes it easy to find top-rated businesses based on customer reviews and expert
-            recommendations. Whether you're looking for trusted services, shopping options, or professional solutions, 
-          our comprehensive directory ensures you connect with the best businesses available!
+            Explore the most popular business listings in your area through our
+            local business directory listing, highly rated by locals and
+            visitors alike. Our platform makes it easy to find top-rated
+            businesses based on customer reviews and expert recommendations.
+            Whether you're looking for trusted services, shopping options, or
+            professional solutions, our comprehensive directory ensures you
+            connect with the best businesses available!
           </p>
         </div>
         <div className="row justify-content-around gap-2">
-
-
-          {
-            businessData?.map((business) => (
-              <Link
-                to={
-                  business?.selectedPlan?.isPremium
-                    ? `/business/premium/${business?._id}`
-                    : `/business/${business?._id}`
-                }
-                key={business._id}
-                className="text-decoration-none text-dark col-12 col-md-5 b-theme location-card mt-3 business-card"
+          {businessData?.map((business) => (
+            <Link
+              to={
+                business?.selectedPlan?.isPremium
+                  ? `/business/premium/${slugify(
+                      business.businessName
+                    )}/${business?._id}`
+                  : `/business/${slugify(
+                      business.businessName
+                    )}/${business?._id}`
+              }
+              key={business._id}
+              className="text-decoration-none text-dark col-12 col-md-5 b-theme location-card mt-3 business-card"
+            >
+              <div
+                className="row p-2"
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                }}
               >
+                <div className="col-4 p-0">
+                  <img
+                    src={
+                      business?.logo && business?.logo?.length > 0
+                        ? business?.logo
+                        : Placeholder
+                    }
+                    alt=""
+                    className="w-100 br-theme"
+                    style={{ objectFit: "cover" }} // Ensure image is responsive
+                  />
+                </div>
                 <div
-                  className="row p-2"
+                  className="col-8"
                   style={{
                     display: "flex",
-                    flexDirection: "row",
-                    flexWrap: "wrap",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    height: "100%",
                   }}
                 >
-                  <div className="col-4 p-0">
-                    <img
-                      src={
-                        business?.logo && business?.logo?.length > 0
-                          ? business?.logo
-                          : Placeholder
-                      }
-                      alt=""
-                      className="w-100 br-theme"
-                      style={{ objectFit: "cover" }} // Ensure image is responsive
-                    />
-                  </div>
-                  <div
-                    className="col-8"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "space-between",
-                      height: "100%",
-                    }}
-                  >
-                    <div className="col-12 mb-2 mt-2">
-                      <h2
-                        style={{
-                          fontSize: "28px",
-                          whiteSpace: "nowrap",
-                          overflow: "hidden",
-                          textOverflow: "ellipsis",
-                        }}
-                      >
-                        {business?.businessName}
-                      </h2>
-                    </div>
-                    <div className="col-12">
-                      <span>{business?.category?.name}</span>
-                    </div>
-                    <div
-                      className="col-12 mt-3"
+                  <div className="col-12 mb-2 mt-2">
+                    <h2
                       style={{
-                        display: "-webkit-box",
-                        WebkitLineClamp: 3, // Limit to 3 lines
-                        WebkitBoxOrient: "vertical",
+                        fontSize: "28px",
+                        whiteSpace: "nowrap",
                         overflow: "hidden",
                         textOverflow: "ellipsis",
                       }}
                     >
-                      <h3 className="fs-16">
-                        <i className="bi bi-crosshair"></i>
-                        <span className="ms-1 fs-15">
-                          {business?.address?.buildingName},{" "}
-                          {business?.address?.city},{" "}
-                          {business?.address?.landMark},{" "}
-                          {business?.address?.streetName},{" "}
-                          {business?.address?.state}
-                        </span>
-                      </h3>
-                    </div>
+                      {business?.businessName}
+                    </h2>
+                  </div>
+                  <div className="col-12">
+                    <span>{business?.category?.name}</span>
+                  </div>
+                  <div
+                    className="col-12 mt-3"
+                    style={{
+                      display: "-webkit-box",
+                      WebkitLineClamp: 3, // Limit to 3 lines
+                      WebkitBoxOrient: "vertical",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                  >
+                    <h3 className="fs-16">
+                      <i className="bi bi-crosshair"></i>
+                      <span className="ms-1 fs-15">
+                        {business?.address?.buildingName},{" "}
+                        {business?.address?.city}, {business?.address?.landMark}
+                        , {business?.address?.streetName},{" "}
+                        {business?.address?.state}
+                      </span>
+                    </h3>
+                  </div>
 
-                    {/* Rating Section (on the right side) */}
-                    <div className="col-12 mt-3 d-flex justify-content-end align-items-center">
-                      <div
-                        className="d-flex align-items-center"
+                  {/* Rating Section (on the right side) */}
+                  <div className="col-12 mt-3 d-flex justify-content-end align-items-center">
+                    <div
+                      className="d-flex align-items-center"
+                      style={{
+                        backgroundColor: "white", // White background
+                        borderRadius: "10px",
+                        padding: "5px 10px",
+                        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Subtle shadow for clean look
+                      }}
+                    >
+                      {/* Always show a yellow star */}
+                      <FaStar color="gold" size={20} />
+                      <span
+                        className="ms-2"
                         style={{
-                          backgroundColor: "white", // White background
-                          borderRadius: "10px",
-                          padding: "5px 10px",
-                          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Subtle shadow for clean look
+                          fontWeight: "bold",
+                          fontSize: "16px",
+                          color: "#d48a27", // Dark gold color for the rating count
                         }}
                       >
-                        {/* Always show a yellow star */}
-                        <FaStar color="gold" size={20} />
-                        <span
-                          className="ms-2"
-                          style={{
-                            fontWeight: "bold",
-                            fontSize: "16px",
-                            color: "#d48a27", // Dark gold color for the rating count
-                          }}
-                        >
-                          {business?.rating > 0 ? business?.rating : 0}{" "}
-                          {/* Show 0 if no rating */}
-                        </span>
-                      </div>
+                        {business?.rating > 0 ? business?.rating : 0}{" "}
+                        {/* Show 0 if no rating */}
+                      </span>
                     </div>
                   </div>
                 </div>
-              </Link>  
-            ))}
+              </div>
+            </Link>
+          ))}
         </div>
         {visibleBusiness < totalBusinessData && (
           <div className="mb-3 mt-5 text-center">
             {loading ? (
               <Spinner
                 style={{
-
                   borderWidth: "0.2em",
                   width: "1.5rem",
                   height: "1.5rem",
                   display: "inline-block",
-                }}  
+                }}
                 variant="primary"
               />
             ) : (
@@ -160,10 +167,6 @@ const BusinessesSection = ({
               </button>
             )}
           </div>
-
-
-
-
         )}
       </div>
     </section>
