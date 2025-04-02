143ec007a946ff9d63d5dcc197f633b0b646594b
Gurupriyan j <gurupriyanj007@gmail.com>
2025-04-03 02:45:01 +0530

    update the og data link

--- frontend/src/components/ShareButton.jsx
+++ frontend/src/components/ShareButton.jsx
@@ -73,7 +73,7 @@ const ShareButton = ({ profileId,number, countryCode,logoUrl,businessName,theme,
   }, [url, showQRCode]);
 
   const generateShareLink = () => {
-    return ` https://auxxbay.auxxweb.in/profile/${slugify(businessName)}/${profileId}`;  
+    return ` https://server.auxxweb.in/profile/${slugify(businessName)}/${profileId}`;  
 };
 
 
